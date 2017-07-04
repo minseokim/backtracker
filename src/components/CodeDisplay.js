33fd8608204dc25e37da6b541c0aeea971ad0373
minseokim <minseoalexkim@gmail.com>
2017-07-04 13:11:19 -0400

    Refactor into container component for ParenthesesGeneratorDisplay

--- src/components/CodeDisplay.js
+++ src/components/CodeDisplay.js
@@ -1,22 +1,21 @@
 import React from "react";
 
-const CodeDisplay = (props) => {
+const CodeDisplay = props => {
   const { sourceCode, line } = props;
 
   return (
     <div className="CodeDisplay">
       <pre className="CodeDisplay__pre">
         <ol>
-          {sourceCode.split('\n').map((fnLine, index) => {
+          {sourceCode.split("\n").map((fnLine, index) => {
             return (
               <li
                 key={index}
-                style={ {backgroundColor : index === line ? 'orange' : 'white'}}
+                style={{ backgroundColor: index === line ? "orange" : "white" }}
               >
                 {fnLine}
               </li>
-            )
-
+            );
           })}
         </ol>
       </pre>
